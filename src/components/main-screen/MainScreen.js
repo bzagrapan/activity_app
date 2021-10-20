6d52ce5500af35b2e1f9bae2f8bf84e33c233e8a
Branislav Zagrapan <branislav.zagrapan@dxc.com>
2021-10-20 16:36:39 +0200

    table like design

--- src/components/main-screen/MainScreen.js
+++ src/components/main-screen/MainScreen.js
@@ -1,8 +1,10 @@
-import { AnchorButton, Card } from '@blueprintjs/core';
+import { AnchorButton } from '@blueprintjs/core';
 import _ from 'lodash';
 import React, { useState } from 'react';
+import { Container, Row, Col } from 'react-grid-system';
 import { getActivity } from '../../utils/API';
 import './style.css';
+import * as dayjs from 'dayjs';
 
 const MainScreen = (props) => {
     const [isLoading, setIsLoading] = useState(false);
@@ -11,52 +13,84 @@ const MainScreen = (props) => {
 
     const getNewActivity = () => {
         setIsLoading(true);
-        getActivity().then((activity) => {
-            setActivities([...activities, activity]);
-            setIsLoading(false);
-        });
+        getActivity()
+            .then((activity) => {
+                setActivities([...activities, activity]);
+                setIsLoading(false);
+            })
+            .catch((e) => {
+                console.log(e);
+            });
     };
 
     const displayActivities = () => {
         return activities.map((activity) => {
             return (
-                <Card elevation={2} key={activity.key} className="activity-card">
-                    <div className="activity-detail-row">
-                        <b>ACTIVITY: </b>
-                        <span>{activity.activity}</span>
-                    </div>
-                    <div className="activity-detail-row">
-                        <b>TYPE: </b>
-                        <span>{activity.type}</span>
-                    </div>
-                    <div className="activity-detail-row">
-                        <b>PRICE: </b>
-                        <span>{activity.price}</span>
-                    </div>
-                    <div>
-                        <b>PARTICIPANTS: </b>
-                        <span>{activity.participants}</span>
-                    </div>
-                </Card>
+                <tr key={activity.key}>
+                    <td>{activity.createdAt}</td>
+                    <td>{activity.type}</td>
+                    <td>{activity.participants}</td>
+                    <td>{activity.price}</td>
+                    <td>{activity.activity}</td>
+                    <td>DELETE ICON</td>
+                </tr>
             );
         });
     };
 
     return (
-        <div className="app-container">
-            <div className="app-header">Activity application</div>
-            <hr />
-            <div className="control-bar">
-                <AnchorButton
-                    loading={isLoading}
-                    disabled={isLoading}
-                    text="Add activity"
-                    onClick={getNewActivity}
-                    intent="primary"
-                    className="add-activity-btn"
-                />
-            </div>
-            <div className="activity-container">{displayActivities()}</div>
+        <div>
+            <Container fluid className="app-container">
+                <Row>
+                    <Col lg={1} md={1}></Col>
+                    <Col lg={10} md={10}>
+                        <div className="app-header">Activity list</div>
+                        <hr />
+                    </Col>
+                    <Col lg={1} md={1}></Col>
+                </Row>
+                <Row>
+                    <Col lg={1} md={1}></Col>
+                    <Col lg={10} md={10}>
+                        Filters
+                        <hr />
+                    </Col>
+                    <Col lg={1} md={1}></Col>
+                </Row>
+                <Row>
+                    <Col lg={1} md={1}></Col>
+                    <Col lg={10} md={10} className="activity-col">
+                        <table className="activity-table">
+                            <thead>
+                                <tr>
+                                    <th>Created at</th>
+                                    <th>Type</th>
+                                    <th>Participants</th>
+                                    <th>Price</th>
+                                    <th>Activity</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>{displayActivities()}</tbody>
+                        </table>
+                    </Col>
+                    <Col lg={1} md={1}></Col>
+                </Row>
+                <Row>
+                    <Col lg={1} md={1}></Col>
+                    <Col lg={10} md={10}>
+                        <AnchorButton
+                            loading={isLoading}
+                            disabled={isLoading}
+                            text="Add activity"
+                            onClick={getNewActivity}
+                            intent="primary"
+                            className="add-activity-btn"
+                        />
+                    </Col>
+                    <Col lg={1} md={1}></Col>
+                </Row>
+            </Container>
         </div>
     );
 };
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,11 +1,14 @@
 import axios from 'axios';
+import * as dayjs from 'dayjs';
 
 export const getActivity = () => {
     return new Promise(function (resolve, reject) {
         axios
             .get('https://www.boredapi.com/api/activity')
             .then((response) => {
-                return resolve(response.data);
+                let res = response.data;
+                res.createdAt = dayjs().format('HH:mm:ss');
+                return resolve(res);
             })
             .catch((e) => {
                 return reject(e);
